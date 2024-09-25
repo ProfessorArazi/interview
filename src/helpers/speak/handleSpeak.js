2ffcfb538cd42b17866a2533ea32e046278b347d
amit arazi <amitarazi12345@gmail.com>
2024-09-25 19:57:00 +0300

    voice

--- src/helpers/speak/handleSpeak.js
+++ src/helpers/speak/handleSpeak.js
@@ -4,62 +4,62 @@ const synth = window.speechSynthesis;
 const utterance = new SpeechSynthesisUtterance();
 
 export const handleSpeak = async (type, speakData) => {
-  const {
-    setDisableButton,
-    speed,
-    setSpeed,
-    playerRef,
-    setQuestion,
-    setLoading,
-    communitySubjects,
-    customSubjects,
-    voices,
-  } = speakData;
+    const {
+        setDisableButton,
+        speed,
+        setSpeed,
+        playerRef,
+        setQuestion,
+        setLoading,
+        communitySubjects,
+        customSubjects,
+        voices,
+    } = speakData;
 
-  setDisableButton(true);
-  if (!handleSpeak.didrun) {
-    setLoading(true);
-    handleSpeak.didrun = true;
-  }
-  try {
-    const question = askQuestion(type, communitySubjects, customSubjects);
-    utterance.text = question;
-    if (voices.length && question.match(/[a-z]/gi)) {
-      const voice = voices.find(
-        (voice) =>
-          voice.voiceURI === "Microsoft David - English (United States)"
-      );
-      if (voice) {
-        utterance.voice = voice;
-        utterance.lang = "en-US";
-      }
+    setDisableButton(true);
+    if (!handleSpeak.didrun) {
+        setLoading(true);
+        handleSpeak.didrun = true;
     }
+    try {
+        const question = askQuestion(type, communitySubjects, customSubjects);
+        utterance.text = question;
 
-    if (!+speed.trim()) {
-      await setSpeed("1");
-      utterance.rate = "1";
-    } else if (+speed.trim() > 2) {
-      await setSpeed("2");
-      utterance.rate = "2";
-    } else utterance.rate = speed || "1";
-    utterance.onend = () => {
-      playerRef.current?.stop();
-      setDisableButton(false);
-    };
-    
-    const speechDuration = (question.split(" ").length) * 1000;
-    
-    utterance.onstart = () => {
-      playerRef.current?.play();
-      setTimeout(() => {
-        playerRef.current?.stop();
-      }, speechDuration * 0.4);
-      
-      setLoading(false);
-    };
-    synth.speak(utterance);
-    setQuestion(question);
-  } catch (e) {
-    setLoading(false);
-  }
+        if (voices.length && question.match(/[a-z]/gi)) {
+            const voice = voices.find(
+                (voice) => voice.voiceURI === "Google UK English Male"
+            );
+            if (voice) {
+                utterance.voice = voice;
+                utterance.lang = "en-US";
+            }
+        }
+
+        if (!+speed.trim()) {
+            await setSpeed("1");
+            utterance.rate = "1";
+        } else if (+speed.trim() > 2) {
+            await setSpeed("2");
+            utterance.rate = "2";
+        } else utterance.rate = speed || "1";
+        utterance.onend = () => {
+            playerRef.current?.stop();
+            setDisableButton(false);
+        };
+
+        const speechDuration = question.split(" ").length * 1000;
+
+        utterance.onstart = () => {
+            playerRef.current?.play();
+            setTimeout(() => {
+                playerRef.current?.stop();
+            }, speechDuration * 0.4);
+
+            setLoading(false);
+        };
+        synth.speak(utterance);
+        setQuestion(question);
+    } catch (e) {
+        setLoading(false);
+    }
 };
